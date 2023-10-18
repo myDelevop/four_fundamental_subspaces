c21c4021f964b4ef4178f305271f118742a2ece3
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-10-18 06:32:33 +0200

    empty object definition

--- four_fundamental_subspaces.py
+++ four_fundamental_subspaces.py
@@ -35,5 +35,29 @@ if __name__ == '__main__':
     elements for the pivotal element (because there is a reduction of the Identity
     Matrix. That's why we take the elements :rank_A (from 0 to rank_A)
     """
-    # The indices where pivots appear in the matrix A
+    # The indices where pivots appear in the matrix A (we stop to rank of A because there are more)
     pivot_idx = np.array(rref_augmented[1][:rank_a])
+
+    matrix_computation = {
+        "matrix": matrix,
+        "shape": (m, n),
+        "num_rows": m,
+        "num_cols": n,
+        "computation_time_rank": 0,
+        "computation_time_rref": 0,
+        "computation_time_tot": 0,
+        "rank": rank_a,
+        "identity": identity,
+        "augmented_matrix": augmented_matrix,
+        "rref_augmented": rref_augmented,
+        "a_rref": a_rref,
+        "id_rref": id_rref,
+        "pivot_idx": pivot_idx,
+        "pivots_tmp": pivots_tmp,
+        "four_subspaces": {
+            "Range_A": {"span": []},
+            "Range_AT": {"span": []},
+            "NULL_A": {"span": []},
+            "NULL_AT": {"span": []},
+        }
+    }
