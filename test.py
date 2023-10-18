0d8e0f6a9863cb905c2dbbc76fea5de855c55d59
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-10-18 08:24:01 +0200

    test algo complexity

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 import random
 from sympy.matrices import Matrix, eye
 
-if __name__ == '__main__':
-    m = random.randint(48, 52)
-    n = random.randint(48, 52)
-    matrix = Matrix(m, n, lambda i, j: random.randint(-106, 106))
 
-    #    rank_a = matrix.rank()
-    #    identity = eye(m)
-    # augmented_matrix = matrix.row_join(identity)
-    # rref_augmented = augmented_matrix.rref()
-    matrix.rref()
+if __name__ == '__main__':
+
+    m = random.randint(58, 62)
+    n = random.randint(58, 62)
+    A = Matrix(m, n, lambda i, j: random.randint(-14, 14))
+
+    A.rref()
