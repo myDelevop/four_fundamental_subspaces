0a0a8850797c3a56b69f79c8f427c1a0861c1028
Rocco Caliandro <rocco.caliandro@toptal.com>
2023-10-18 06:23:01 +0200

    compute shape of matrix

--- four_fundamental_subspaces.py
+++ four_fundamental_subspaces.py
@@ -1,4 +1,14 @@
+from sympy.matrices import Matrix
 
 
 if __name__ == '__main__':
-    print("Hello")
+
+    # Define a static Matrix A
+    A = Matrix([
+        [1, 2, 2, 3],
+        [2, 4, 1, 3],
+        [3, 6, 1, 4]
+    ])
+
+    # m is the number of rows of the matrix and n is the number of columns
+    (m, n) = A.shape
